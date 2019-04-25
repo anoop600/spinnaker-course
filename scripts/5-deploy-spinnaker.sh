dc5163d53ad957f00c42d1c338fed02e74b79409
Anoop Jain <anoop.jain10@gmail.com>
2019-04-25 17:47:24 +0530

    Update 5-deploy-spinnaker.sh

--- scripts/5-deploy-spinnaker.sh
+++ scripts/5-deploy-spinnaker.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
-
-#install xrdp in AZURE
-
-sudo apt-get update -y
-sudo apt-get install xfce4 -y
-sudo apt-get install xrdp -y
-sudo systemctl enable xrdp
-echo xfce4-session >~/.xsession
-sudo service xrdp restart
-
-# install dependencies
-sudo apt update
-sudo apt-get -y install redis-server
-sudo systemctl enable redis-server
-sudo systemctl start redis-server
-
-echo 'spinnaker.s3.versioning: false' > ~/.hal/default/profiles/front50-local.yml
-
 # env flag that need to be set:
-SPINNAKER_VERSION=1.12.6
+SPINNAKER_VERSION=1.13.5
 
 set -e
 

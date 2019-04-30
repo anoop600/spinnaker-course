b124a346231af1f66cb8577bdd27f185d5001877
Ubuntu <ubuntu@ip-172-31-14-128.ap-southeast-1.compute.internal>
2019-04-30 11:29:54 +0000

    edited permission

--- scripts/3-install-halyard.sh
+++ scripts/3-install-halyard.sh
@@ -5,8 +5,6 @@ set -e
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
 sudo bash InstallHalyard.sh --user ubuntu
 
-sudo update-halyard
-
 sudo apt-get update -y
 
 sudo apt-get install \
@@ -23,16 +21,16 @@ sudo apt-get install docker.io -y
 
 sudo usermod -aG docker ubuntu
 
-#sudo docker run -p 127.0.0.1:9090:9000 -d --name minio1 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data
+sudo docker run -p 127.0.0.1:9090:9000 -d --name minio1 -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data
 
 sudo apt-get -y install jq apt-transport-https
 
-#MINIO_SECRET_KEY=`echo $(sudo docker exec minio1 cat /data/.minio.sys/config/config.json) |jq -r '.credential.secretKey'`
-#MINIO_ACCESS_KEY=`echo $(sudo docker exec minio1 cat /data/.minio.sys/config/config.json) |jq -r '.credential.accessKey'`
+MINIO_SECRET_KEY=`echo $(sudo docker exec minio1 cat /data/.minio.sys/config/config.json) |jq -r '.credential.secretKey'`
+MINIO_ACCESS_KEY=`echo $(sudo docker exec minio1 cat /data/.minio.sys/config/config.json) |jq -r '.credential.accessKey'`
 
-#echo $MINIO_SECRET_KEY | hal config storage s3 edit --endpoint http://127.0.0.1:9090 \
-#    --access-key-id $MINIO_ACCESS_KEY \
-#    --secret-access-key
+echo $MINIO_SECRET_KEY | hal config storage s3 edit --endpoint http://127.0.0.1:9090 \
+    --access-key-id $MINIO_ACCESS_KEY \
+    --secret-access-key
 
-#hal config storage edit --type s3
-hal config storage edit --type redis
+hal config storage edit --type s3
+#hal config storage edit --type redis
